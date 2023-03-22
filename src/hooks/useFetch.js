b1ac8322a9a25618725e4f204b316fd320099845
adailtonygor@hotmail.com <Nova@NOVARJLAP-029>
2023-03-22 15:45:28 -0300

    first commit

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,19 @@
+import { useEffect, useState, setState } from "react";
+
+export const useFetch = (url, method = 'get') => {
+    const [response, setResponse] = useState({
+        data: null,
+        loading: true
+    })
+
+    useEffect(function () {
+        fetch(url, { method })
+            .then(resp => resp.json())
+            .then(json => setState({
+                data: json,
+                loading: false
+            }))
+    }, [url, method])
+
+    return response
+}
